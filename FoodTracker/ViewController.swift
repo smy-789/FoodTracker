496d7d052bf73fb77bdfcd3689387cb875508d51
smy-789 <smy9905@163.com>
2021-03-17 14:41:54 +0800

    FoodTracker First Commit 学习构建用户基本界面

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -5,10 +5,21 @@
 //  Created by 盛明钰 on 2021/3/17.
 //
 
+// 用户界面的行为将在此类中定义
+
 import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties 标记：属性
+    /* 以字符//MARK:开头的注释是一种特殊类型的注释，用于组织您的代码，并帮助您（以及任何阅读代码的人）浏览代码。你稍后会看到这个在起作用。具体而言，您添加的注释表明这是代码中列出属性的部分。
+     */
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
